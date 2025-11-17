ce1a95d166aa67f3a97f1f3d8129f2ae9fb6e471
Ayush Tiwari <ayushzztiwari69@gmail.com>
2025-11-17 18:59:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-import streamlit as st
+from flask import Flask, render_template, request
 
-# Streamlit UI
-st.title("Power Calculator")
-st.write("Enter a number to calculate its square, cube, and fifth power.")
+app = Flask(__name__)
 
-# Get user input
-n = st.number_input("Enter an integer", value=1, step=1)
+@app.route('/')
+def index():
+    return '''
+        <html>
+        <body>
+            <form action="/greet" method="POST">
+                Enter your name: <input type="text" name="username">
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+        </html>
+    '''
 
-# Calculate results
-square = n ** 2
-cube = n ** 3
-fifth_power = n ** 5
+@app.route('/greet', methods=['POST'])
+def greet():
+    user_input = request.form['username']
+    return f"Hello {user_input}, Welcome to this app for Docker demonstration."
 
-# Display results
-st.write(f"The square of {n} is: {square}")
-st.write(f"The cube of {n} is: {cube}")
-st.write(f"The fifth power of {n} is: {fifth_power}")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
